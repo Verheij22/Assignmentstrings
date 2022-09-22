cf2f34ac8115e5f0c49cf1e4c05f844c74c2bfb6
Aerodyne <>
2022-09-22 03:23:58 +0200

    latest changes Assignment strings

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ __human_name__ = 'strings'
 # Add your code after this line
 
     #part 1
-player_1 = 'Ruud Gullit '      
-player_2 = 'Marco van Basten '
+player_0 = 'Ruud Gullit '      
+player_1 = 'Marco van Basten '
 
 goal_0 = 32
 goal_1 = 54
 
-scorers = player_1 + str (goal_0) + ', ' + player_2 + str (goal_1)
-report = f'{player_1}scored in the {goal_0}nd minute\n{player_2}scored in the {goal_1}th minute'
+scorers = player_0 + str (goal_0) + ', ' + player_1 + str (goal_1)
+report = f'{player_0}scored in the {goal_0}nd minute\n{player_1}scored in the {goal_1}th minute'
 print (report)
    
     #part 2
@@ -22,15 +22,16 @@ last_name = player[player.find(' '):]
 print (first_name) 
  
 last_name_len = len(player[player.find(' '):-1])
+first_name_len = len(player[:player.find(' ')])
 print (last_name_len)
 
-name_short = (player[0] + '.' + last_name)
+name_short = player[0] + '.' + last_name
 print (name_short)
 
 yell = first_name + '! '
-chant = yell *3
+chant = yell * first_name_len 
 chant = chant.rstrip(chant[-1])
 print (chant)
 
-good_chant = (chant[-1] != ' ')
+good_chant = chant[-1] != ' '
 print (good_chant)
